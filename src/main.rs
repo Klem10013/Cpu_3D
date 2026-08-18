ee761064b5dea1c967814c2e81a22976e37802cf
Klem1 <clement.bataille@epita.fr>
2026-08-18 02:25:07 +0200

    it work

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,27 @@ const WIDTH2I : isize = (WIDTH as isize) /2;
 const HEIGHT: usize = 500;
 const HEIGHT2I : isize = (HEIGHT as isize) /2;
 const VIEW_DISTANCE : isize = 1000000;
-const FPS : usize = 60;
 
 #[derive(Debug)]
 struct Camera{
     focal_dist : isize,
+    focal_distf : f64,
     pos : (isize,isize,isize),
+    posf : (f64,f64,f64),
     rot : (f64,f64,f64),
     normal: Normal,
-    sun : (isize,isize,isize),
+    sun : (f64,f64,f64),
     lights : Vec<(isize,isize,isize)>,
 }
 
 impl Camera{
-    pub const fn new(focal_point_distance : isize,sun : (isize,isize,isize), lights : Vec<(isize,isize,isize)>) -> Camera{
+    pub const fn new(focal_point_distance : isize,sun : (f64,f64,f64), lights : Vec<(isize,isize,isize)>) -> Camera{
         Camera{
             //focal_point: (0, 0, -focal_point_distance),
             focal_dist: focal_point_distance,
+            focal_distf: focal_point_distance as f64,
             pos : (0, 0, 0),
+            posf: (0.0, 0.0, 0.0),
             rot : (0f64, 0f64, 0f64),
             normal: Normal::new_from_equation((0,0,1), (0,0,focal_point_distance)),
             sun,
@@ -43,6 +46,9 @@ impl Camera{
         self.pos.0 +=  x;
         self.pos.1 += direction.1;
         self.pos.2 += z;
+        self.posf.0 = self.pos.0 as f64;
+        self.posf.1 = self.pos.1 as f64;
+        self.posf.2 = self.pos.2 as f64;
     }
 }
 
@@ -51,21 +57,43 @@ struct Normal{
     nx : isize,
     ny : isize,
     nz : isize,
+    nxf : f64,
+    nyf : f64,
+    nzf : f64,
     constant : isize,
+    constantf : f64
 }
 
 impl Normal{
-    pub const fn new(p1 : (isize,isize,isize), p2 : (isize,isize,isize), p3 : (isize,isize,isize)) -> Normal{
-        let a: (isize, isize, isize) = (p2.0 - p1.0, p2.1 - p1.1, p2.2 - p1.2);
-        let b: (isize, isize, isize) = (p3.0 - p1.0, p3.1 - p1.1, p3.2 - p1.2);
+    pub const fn new(p1 : (f64,f64,f64), p2 : (f64,f64,f64), p3 : (f64,f64,f64)) -> Normal{
+        let a = (p2.0 - p1.0, p2.1 - p1.1, p2.2 - p1.2);
+        let b = (p3.0 - p1.0, p3.1 - p1.1, p3.2 - p1.2);
         let nx = a.1* b.2 - a.2* b.1;
         let ny = a.2* b.0 - a.0* b.2;
         let nz = a.0* b.1 - a.1* b.0;
+        let constant = -1f64 * (nx*p1.0 + ny*p1.1 + nz*p1.2);
         Normal {
-            nx ,
-            ny ,
-            nz ,
-            constant : -1 * (nx*p1.0 + ny*p1.1 + nz*p1.2),
+            nxf : nx,
+            nx  : nx as isize ,
+            nyf : ny,
+            ny  : ny as isize,
+            nzf : nz,
+            nz  : nz as isize,
+            constantf: constant,
+            constant: constant as isize,
+        }
+    }
+
+    pub const fn new_from_equation_f64(eq : (f64,f64,f64), pt : (f64,f64,f64)) -> Normal{
+        Normal{
+            nx: eq.0 as isize,
+            ny: eq.1 as isize,
+            nz: eq.2 as isize,
+            nxf: eq.0,
+            nyf: eq.1,
+            nzf: eq.2,
+            constant: -1 * (eq.0*pt.0 + eq.1*pt.1 + eq.2*pt.2) as isize,
+            constantf: -1f64 * (eq.0*pt.0 + eq.1*pt.1 + eq.2*pt.2),
         }
     }
 
@@ -74,7 +102,11 @@ impl Normal{
             nx: eq.0,
             ny: eq.1,
             nz: eq.2,
+            nxf: eq.0 as f64,
+            nyf: eq.1 as f64,
+            nzf: eq.2 as f64,
             constant: -1 * (eq.0*pt.0 + eq.1*pt.1 + eq.2*pt.2),
+            constantf: (-1 * (eq.0*pt.0 + eq.1*pt.1 + eq.2*pt.2)) as f64,
         }
     }
 
@@ -83,9 +115,8 @@ impl Normal{
         self.nx*vec.0 + self.ny*vec.1 + self.nz*vec.2
     }
 
-    pub const fn dot_product_f64(&self, vec:(isize,isize,isize)) -> f64{
-        let (x,y,z) = vec;
-        (x as f64) * (self.nx as f64) + (y as f64) * (self.ny as f64) + (z as f64) * (self.nz as f64)
+    pub const fn dot_product_f64(&self, vec:(f64,f64,f64)) -> f64{
+        vec.0 * self.nxf +vec.1 * self.nyf +vec.2 * self.nzf
     }
 
     #[inline(always)]
@@ -93,19 +124,23 @@ impl Normal{
         self.nx *= -1;
         self.ny *= -1;
         self.nz *= -1;
+        self.nxf *= -1.0;
+        self.nyf *= -1.0;
+        self.nzf *= -1.0;
         self.constant *= -1;
+        self.constantf *= -1f64;
     }
 
     #[inline(always)]
-    pub const fn to_vec(&self) -> (isize,isize,isize){
-        (self.nx,self.ny,self.nz)
+    pub const fn to_vec_f64(&self) -> (f64,f64,f64){
+        (self.nxf,self.nyf,self.nzf)
     }
 }
 
 struct Triangle3D{
-    p1 : (isize,isize,isize),
-    p2 : (isize,isize,isize),
-    p3 : (isize,isize,isize),
+    p1 : (f64,f64,f64),
+    p2 : (f64,f64,f64),
+    p3 : (f64,f64,f64),
     color : u32,
     inverse_normal : bool,
     normal : Normal,
@@ -113,7 +148,7 @@ struct Triangle3D{
 }
 
 impl Triangle3D{
-    pub fn new(p1 : (isize,isize,isize), p2 : (isize,isize,isize), p3 : (isize,isize,isize), color : u32, only_face : bool,inverse_normal : bool) -> Triangle3D{
+    pub fn new(p1 : (f64,f64,f64), p2 : (f64,f64,f64), p3 : (f64,f64,f64), color : u32, only_face : bool,inverse_normal : bool) -> Triangle3D{
         let mut normal = Normal::new(p1,p2,p3);
         if inverse_normal{
             normal.inverse();
@@ -121,12 +156,24 @@ impl Triangle3D{
         Triangle3D{p1 ,p2 ,p3 ,color , inverse_normal, normal, only_face }
     }
 
+    pub fn new_stl(p1:(f64,f64,f64), p2: (f64,f64,f64), p3 : (f64,f64,f64),normal: Normal)-> Triangle3D{
+        Triangle3D{
+            p1,
+            p2,
+            p3,
+            color: 0xFFFFFF,
+            inverse_normal: false,
+            normal,
+            only_face: false,
+        }
+    }
+
     pub fn to_2d(&self, camera: &Camera) ->  Vec<Triangle2D>{
         let p1_3d = point_transformation(self.p1, camera);
         let p2_3d = point_transformation(self.p2, camera);
         let p3_3d = point_transformation(self.p3, camera);
 
-        if p1_3d.2 < camera.focal_dist && p2_3d.2 < camera.focal_dist && p3_3d.2 < camera.focal_dist {
+        if p1_3d.2 < camera.focal_distf && p2_3d.2 < camera.focal_distf && p3_3d.2 < camera.focal_distf {
             vec![]
         }else {
             let mut triangles = vec![];
@@ -134,15 +181,15 @@ impl Triangle3D{
             if self.inverse_normal{
                 normal.inverse()
             }
-            let p1;
-            let p2;
-            let p3;
-            if p1_3d.2 < camera.focal_dist {
-                if p2_3d.2 < camera.focal_dist {
+            let p1 : (f64,f64);
+            let p2 : (f64,f64);
+            let p3 : (f64,f64);
+            if p1_3d.2 < camera.focal_distf {
+                if p2_3d.2 < camera.focal_distf {
                     p1 = plane_clipping(p1_3d,p3_3d,&camera.normal);
                     p2 = plane_clipping(p2_3d,p3_3d,&camera.normal);
                     p3 = projection(p3_3d,&camera.normal);
-                }else if p3_3d.2 < camera.focal_dist {
+                }else if p3_3d.2 < camera.focal_distf {
                     p1 = plane_clipping(p1_3d,p2_3d,&camera.normal);
                     p2 = projection(p2_3d,&camera.normal);
                     p3 = plane_clipping(p3_3d,p2_3d,&camera.normal);
@@ -152,10 +199,10 @@ impl Triangle3D{
                     p2 = projection(p2_3d,&camera.normal);
                     p3 = projection(p3_3d,&camera.normal);
                     p1 = pr2;
-                    triangles.push(Triangle2D::new(pr2,pr3,p3,normal.clone(),self.color,self.only_face));
+                    triangles.push(Triangle2D::new(pr2,pr3,p3,normal.clone(),self.only_face));
                 }
-            }else if p2_3d.2 < camera.focal_dist {
-                if p3_3d.2 < camera.focal_dist{
+            }else if p2_3d.2 < camera.focal_distf {
+                if p3_3d.2 < camera.focal_distf{
                     p1 = projection(p1_3d,&camera.normal);
                     p2 = plane_clipping(p2_3d,p1_3d,&camera.normal);
                     p3 = plane_clipping(p3_3d,p1_3d,&camera.normal);
@@ -165,82 +212,85 @@ impl Triangle3D{
                     p1 = projection(p1_3d,&camera.normal);
                     p3 = projection(p3_3d,&camera.normal);
                     p2 = pr1;
-                    triangles.push(Triangle2D::new(pr1,pr3,p3,normal.clone(),self.color,self.only_face));
+                    triangles.push(Triangle2D::new(pr1,pr3,p3,normal.clone(),self.only_face));
                 }
-            }else if p3_3d.2 < camera.focal_dist {
+            }else if p3_3d.2 < camera.focal_distf {
                 let pr1 = plane_clipping(p1_3d,p3_3d,&camera.normal);
                 let pr2 = plane_clipping(p2_3d,p3_3d,&camera.normal);
                 p1 = projection(p1_3d,&camera.normal);
                 p2 = projection(p2_3d,&camera.normal);
                 p3 = pr1;
-                triangles.push(Triangle2D::new(pr1,pr2,p2,normal.clone(),self.color,self.only_face));
+                triangles.push(Triangle2D::new(pr1,pr2,p2,normal.clone(),self.only_face));
             }else{
                 p1 = projection(p1_3d,&camera.normal);
                 p2 = projection(p2_3d,&camera.normal);
                 p3 = projection(p3_3d,&camera.normal);
             }
-            triangles.push(Triangle2D::new(p1, p2, p3, normal, self.color,self.only_face));
+            triangles.push(Triangle2D::new(p1, p2, p3, normal,self.only_face));
             triangles
         }
     }
 }
 #[inline(always)]
-fn plane_clipping(p1 : (isize,isize,isize), p2 : (isize,isize,isize), normal: &Normal) -> (isize,isize){
+fn plane_clipping(p1 : (f64,f64,f64), p2 : (f64,f64,f64), normal: &Normal) -> (f64,f64){
     let vec = (p2.0 - p1.0, p2.1 - p1.1, p2.2 - p1.2);
     intersection_xy(normal, vec, p1)
 }
 
 #[inline(always)]
-fn point_transformation(point : (isize,isize,isize), camera: &Camera,) -> (isize,isize,isize) {
-    let point_c = (point.0 - camera.pos.0, point.1 - camera.pos.1, point.2 - camera.pos.2);
-    let nx = ((point_c.0 as f64) * camera.rot.1.cos() - (point_c.2 as f64) * camera.rot.1.sin()) as isize;
-    let nz = ((point_c.0 as f64) * camera.rot.1.sin() + (point_c.2 as f64) * camera.rot.1.cos()) as isize;
-    (nx, point_c.1, nz)
+fn point_transformation(point : (f64,f64,f64), camera: &Camera,) -> (f64,f64,f64) {
+    let point_c = (point.0 - camera.posf.0, point.1 - camera.posf.1, point.2 - camera.posf.2);
+    let nx     = point_c.0 * camera.rot.1.cos() - point_c.2 * camera.rot.1.sin();
+    let nz_tmp = point_c.0 * camera.rot.1.sin() + point_c.2 * camera.rot.1.cos();
+    let nz     = point_c.1 * camera.rot.2.sin() + nz_tmp * camera.rot.2.cos();
+    let ny     = point_c.1 * camera.rot.2.cos() - nz_tmp * camera.rot.2.sin();
+    (nx, ny, nz)
 }
 
 #[inline(always)]
-const fn projection(point : (isize,isize,isize), normal: &Normal) -> (isize,isize){
-    if is_secant(normal,point){
+const fn projection(point : (f64,f64,f64), normal: &Normal) -> (f64,f64){
+    if is_secant(normal,(point.0 as isize,point.1 as isize, point.2 as isize)) {
             intersection_xy(normal,point,point)
     }else{
-        (0,0)
+        (0f64,0f64)
     }
 }
 
 #[derive(Debug)]
 struct Triangle2D{
-    p1i : (isize,isize), // position
-    p2i : (isize,isize), // position
-    p3i : (isize,isize), // position
+    p1i : (f64,f64), // position
+    p2i : (f64,f64), // position
+    p3i : (f64,f64), // position
     square_x : isize,
     square_y : isize,
     square_width : isize,
     square_height : isize,
     only_face : bool,
     normal: Normal,
-    color : u32
 }
 
 impl Triangle2D{
-    pub const fn new(p1 : (isize,isize), p2 : (isize,isize), p3 : (isize,isize), normal: Normal, color : u32, only_face : bool) -> Triangle2D{
+    pub const fn new(p1i : (f64,f64), p2i : (f64,f64), p3i : (f64,f64), normal: Normal, only_face : bool) -> Triangle2D{
+        let p1 = (p1i.0 as isize,p1i.1 as isize);
+        let p2 = (p2i.0 as isize,p2i.1 as isize);
+        let p3 = (p3i.0 as isize,p3i.1 as isize);
         Triangle2D{
-            p1i: p1,
-            p2i: p2,
-            p3i: p3,
+            p1i,
+            p2i,
+            p3i,
             square_x: min3(p1.0, p2.0, p3.0),
             square_y: min3(p1.1, p2.1, p3.1),
             square_width: max3(p1.0, p2.0, p3.0),
             square_height: max3(p1.1, p2.1, p3.1),
             only_face,
             normal,
-            color,
         }
     }
 }
 
 #[inline(always)]
-const fn sign(p1 : (isize,isize), p2 : (isize,isize), p3 : (isize,isize)) -> isize{
-    (p1.0 - p3.0) * (p2.1 - p3.1) - (p2.0 - p3.0) * (p1.1 - p3.1)
+const fn sign(p1 : (isize,isize), p2 : (f64,f64), p3 : (f64,f64)) -> f64{
+    (p1.0 as f64 - p3.0) * (p2.1 - p3.1) - (p2.0 - p3.0) * (p1.1 as f64 - p3.1)
 }
 
 #[inline(always)]
@@ -267,9 +317,8 @@ const fn is_secant(normal: &Normal, droite : (isize,isize,isize)) -> bool{
 }
 
 #[inline(always)]
-const fn intersection_t(normal : &Normal, droite: (isize,isize,isize), point : (isize,isize,isize)) -> f64{
-    ((-(normal.dot_product(point)  + normal.constant)) as f64)/
-        (normal.dot_product(droite) as f64)
+const fn intersection_t(normal : &Normal, droite: (f64,f64,f64), point : (f64,f64,f64)) -> f64{
+    -(normal.dot_product_f64(point)  + normal.constantf)/ normal.dot_product_f64(droite)
 }
 #[inline(always)]
 const fn intersection_z(value : isize, prod : isize, normal_const : isize) -> isize{
@@ -277,11 +326,11 @@ const fn intersection_z(value : isize, prod : isize, normal_const : isize) -> is
 }
 
 #[inline(always)]
-const fn intersection_xy(normal : &Normal, droite : (isize,isize,isize), point : (isize,isize,isize)) -> (isize,isize){
+const fn intersection_xy(normal : &Normal, droite : (f64,f64,f64), point : (f64,f64,f64)) -> (f64,f64){
     let t = intersection_t(normal, droite, point);
-    let x = (point.0 as f64) + (droite.0 as f64)*t;
-    let y = (point.1 as f64) + (droite.1 as f64)*t;
-    (x as isize, y as isize)
+    let x = point.0 + droite.0*t;
+    let y = point.1 + droite.1*t;
+    (x, y)
 }
 
 #[inline(always)]
@@ -303,8 +352,8 @@ const fn same_signe(x : isize, y : isize) -> bool{
 }
 
 #[inline(always)]
-fn vec_size(vec : (isize,isize,isize)) -> f64{
-    (((vec.0 * vec.0) + (vec.1 * vec.1) + (vec.2 * vec.2)) as f64).sqrt()
+fn vec_size(vec : (f64,f64,f64)) -> f64{
+    ((vec.0 * vec.0) + (vec.1 * vec.1) + (vec.2 * vec.2)).sqrt()
 }
 
 #[inline(always)]
@@ -315,11 +364,14 @@ fn compute_triangle(triangles: &[Triangle3D], camera: &Camera, atomic_buffer: &m
         let dt = -tri_3d.normal.dot_product_f64(camera.sun);
         let mut light = 220;
         if dt != 0f64{
-            let sz = vec_size(camera.sun) * vec_size(tri_3d.normal.to_vec());
+            let sz = vec_size(camera.sun) * vec_size(tri_3d.normal.to_vec_f64());
             let angle = dt/sz;
             if angle > 0f64{
                 light = (255f64 - (255f64*angle)) as u32;
             }
+        }
+        if light > 220{
+            light = 220
         }
         let r = ((tri_3d.color >> 16).saturating_sub(light)) << 16;
         let g = (((tri_3d.color >> 8) & 0x00FF).saturating_sub(light)) << 8;
@@ -331,17 +383,17 @@ fn compute_triangle(triangles: &[Triangle3D], camera: &Camera, atomic_buffer: &m
                 continue;
             }
 
-            let min_x = if tri.square_x > -WIDTH2I { tri.square_x } else { -WIDTH2I };
-            let min_y = if tri.square_y > -HEIGHT2I { tri.square_y } else { -HEIGHT2I };
-            let max_x = if tri.square_width < WIDTH2I { tri.square_width } else { WIDTH2I };
-            let max_y = if tri.square_height < HEIGHT2I { tri.square_height } else { HEIGHT2I };
+            let min_x = if tri.square_x > -WIDTH2I { tri.square_x - 1 } else { -WIDTH2I };
+            let min_y = if tri.square_y > -HEIGHT2I { tri.square_y - 1} else { -HEIGHT2I };
+            let max_x = if tri.square_width < WIDTH2I { tri.square_width + 1 } else { WIDTH2I };
+            let max_y = if tri.square_height < HEIGHT2I { tri.square_height + 1} else { HEIGHT2I };
 
             for y in min_y..max_y {
                 for x in min_x..max_x {
-                    let d1 = sign((x, y), tri.p1i, tri.p2i) as usize;
-                    let d2 = sign((x, y), tri.p2i, tri.p3i) as usize;
-                    let d3 = sign((x, y), tri.p3i, tri.p1i) as usize;
-                    if (d1 ^ d2) & 0x8000000000000000 == 0 && (d1 ^ d3) & 0x8000000000000000 == 0 {
+                    let d1 = sign((x, y), tri.p1i, tri.p2i);
+                    let d2 = sign((x, y), tri.p2i, tri.p3i);
+                    let d3 = sign((x, y), tri.p3i, tri.p1i);
+                    if (d1.signum() == d2.signum()) && (d2.signum() == d3.signum()){
                         let idx = (y + HEIGHT2I) as usize * WIDTH + (x + WIDTH2I) as usize;
                         let dot = tri.normal.dot_product((x,y,camera.focal_dist));
                         if dot != 0 {
@@ -371,6 +423,8 @@ const TRIANGLE_NB : usize = 150;
 
 const FPS_BUFFER_SIZE: usize = 500;
 const FPS_BUFFER_SIZE_128: u128= FPS_BUFFER_SIZE as u128;
+
+const FPS : usize = 250;
 fn main() {
     let mut buffer : Vec<u32> = vec![0; TRIANGLE_NB];
     let mut atomic_buffer : Vec<AtomicIsize> = (0..(WIDTH*HEIGHT)).map(|_| AtomicIsize::new(encode_isize(VIEW_DISTANCE,0))).collect();
@@ -387,19 +441,19 @@ fn main() {
     let mut old_mouse_pos = (0f32,0f32);
     let mut speed = 10;
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        speed = 10;
         if let Some(pos) = window.get_mouse_pos(MouseMode::Discard) {
             if old_mouse_pos == (0f32,0f32) {
                 old_mouse_pos = pos;
             }else {
                 let diff_x = (pos.0 - old_mouse_pos.0) / 5000f32;
+                let diff_y = (pos.1 - old_mouse_pos.1) / 5000f32;
                 camera.rot.1 += diff_x.to_degrees() as f64;
+                camera.rot.2 += diff_y.to_degrees() as f64;
                 old_mouse_pos = (pos.0, pos.1);
             }
         }else{
             old_mouse_pos = (0f32,0f32)
-        }
-        if window.is_key_down(Key::W){
-            camera.rot.1 += 1f64.to_radians();
         }
         if window.is_key_down(Key::LeftShift){
             speed = 100;
--- src/parse_config.rs
+++ src/parse_config.rs
@@ -1,3 +1,4 @@
+use crate::Normal;
 use std::fs::File;
 use std::io::Read;
 use serde::Deserialize;
@@ -5,7 +6,7 @@ use crate::{Triangle3D,Camera};
 
 #[derive(Deserialize)]
 struct Config{
-    sun : (isize,isize,isize),
+    sun : (f64,f64,f64),
     lights  : Vec<(isize,isize,isize)>,
     rectangles : Vec<Rectangle>,
     triangles : Vec<Triangle>,
@@ -14,35 +15,41 @@ struct Config{
 
 #[derive(Deserialize)]
 struct Rectangle{
-    x : (isize,isize,isize),
-    size : (isize,isize,isize),
+    x : (f64,f64,f64),
+    size : (f64,f64,f64),
     color : u32,
     only_face : bool,
     inverse_normal : bool
 }
 #[derive(Deserialize)]
 struct Triangle{
-    x : (isize,isize,isize),
-    y : (isize,isize,isize),
-    z : (isize,isize,isize),
+    x : (f64,f64,f64),
+    y : (f64,f64,f64),
+    z : (f64,f64,f64),
     color : u32,
     only_face : bool,
     inverse_normal : bool
 }
 
-pub fn make_plane(pos : (isize,isize,isize), size : (isize,isize,isize), color : u32, only_face : bool, inverse_normal : bool) -> (Triangle3D,Triangle3D){
-    let mut sz1 = (size.0,0,0);
-    let mut sz2 = (0,size.1,0);
-    if size.0 == 0{
-        sz1 = (0,0,size.2);
-    }else if size.1 == 0{
-        sz2 = (0,0,size.2);
+pub fn make_plane(pos : (f64,f64,f64), size : (f64,f64,f64), color : u32, only_face : bool, inverse_normal : bool) -> (Triangle3D,Triangle3D){
+    let mut sz1 = (size.0,0f64,0f64);
+    let mut sz2 = (0f64,size.1,0f64);
+    if size.0 == 0f64{
+        sz1 = (0f64,0f64,size.2);
+    }else if size.1 == 0f64{
+        sz2 = (0f64,0f64,size.2);
     }
     let face_1 = Triangle3D::new(pos,(pos.0+sz1.0,pos.1+sz1.1,pos.2+sz1.2),(pos.0+size.0,pos.1+size.1,pos.2 + size.2),color,only_face,!inverse_normal);
     let face_2 = Triangle3D::new(pos,(pos.0+sz2.0,pos.1+sz2.1,pos.2+sz2.2),(pos.0+size.0,pos.1+size.1,pos.2 + size.2),color,only_face,inverse_normal);
     (face_1,face_2)
 }
 
+fn stl_line_to_vec(line : &Vec<&str>) -> (f64,f64,f64){
+    let x = str::parse::<f64>(line[2]).unwrap() * 100f64;
+    let y = -str::parse::<f64>(line[3]).unwrap() * 100f64;
+    let z = -str::parse::<f64>(line[1]).unwrap() * 100f64;
+    (x,y,z)
+}
 fn stl_parser(config: &Config) -> Vec<Triangle3D>{
     let mut all_triangles = vec![];
     for stl_file in config.stl_files.iter(){
@@ -51,9 +58,17 @@ fn stl_parser(config: &Config) -> Vec<Triangle3D>{
         file.read_to_string(&mut content).unwrap();
         let lines = content.split("\n").collect::<Vec<&str>>();
         let mut index = 1;
-        while index < lines.len(){
-            let line = lines[index];
-            index += 1
+        while index + 7 < lines.len(){
+            let mut line_normal = lines[index].split_whitespace().collect::<Vec<&str>>();
+            line_normal.remove(0);
+            let nrm = stl_line_to_vec(&line_normal);
+            let p1 = stl_line_to_vec(&lines[index+2].split_whitespace().collect::<Vec<&str>>());
+            let p2 = stl_line_to_vec(&lines[index+3].split_whitespace().collect::<Vec<&str>>());
+            let p3 = stl_line_to_vec(&lines[index+4].split_whitespace().collect::<Vec<&str>>());
+            let normal = Normal::new_from_equation_f64(nrm,p1);
+            all_triangles.push(Triangle3D::new_stl(p1,p2,p3,normal));
+
+            index += 7;
         }
     }
     all_triangles
@@ -73,32 +88,32 @@ pub fn generate_config() -> (Camera,Vec<Triangle3D>){
     }
     for rec in &config.rectangles {
         //top face
-        let (t1,t2) = make_plane(rec.x,(rec.size.0,0,rec.size.2),rec.color,rec.only_face,rec.inverse_normal);
+        let (t1,t2) = make_plane(rec.x,(rec.size.0,0f64,rec.size.2),rec.color,rec.only_face,rec.inverse_normal);
         triangles.push(t1);
         triangles.push(t2);
 
         //front face
-        let (t1,t2) = make_plane(rec.x,(rec.size.0,rec.size.1,0),rec.color,rec.only_face,rec.inverse_normal);
+        let (t1,t2) = make_plane(rec.x,(rec.size.0,rec.size.1,0f64),rec.color,rec.only_face,rec.inverse_normal);
         triangles.push(t1);
         triangles.push(t2);
 
         //left face
-        let (t1,t2) = make_plane(rec.x,(0,rec.size.1,rec.size.2),rec.color,rec.only_face,!rec.inverse_normal);
+        let (t1,t2) = make_plane(rec.x,(0f64,rec.size.1,rec.size.2),rec.color,rec.only_face,!rec.inverse_normal);
         triangles.push(t1);
         triangles.push(t2);
 
         //right face
-        let (t1,t2) = make_plane((rec.x.0 + rec.size.0,rec.x.1,rec.x.2),(0,rec.size.1,rec.size.2),rec.color,rec.only_face,rec.inverse_normal);
+        let (t1,t2) = make_plane((rec.x.0 + rec.size.0,rec.x.1,rec.x.2),(0f64,rec.size.1,rec.size.2),rec.color,rec.only_face,rec.inverse_normal);
         triangles.push(t1);
         triangles.push(t2);
 
         //back face
-        let (t1,t2) = make_plane((rec.x.0,rec.x.1,rec.x.2 + rec.size.2),(rec.size.0,rec.size.1,0),rec.color,rec.only_face,!rec.inverse_normal);
+        let (t1,t2) = make_plane((rec.x.0,rec.x.1,rec.x.2 + rec.size.2),(rec.size.0,rec.size.1,0f64),rec.color,rec.only_face,!rec.inverse_normal);
         triangles.push(t1);
         triangles.push(t2);
 
         //bottom face
-        let (t1,t2) = make_plane((rec.x.0,rec.x.1 + rec.size.1,rec.x.2),(rec.size.0,0,rec.size.2),rec.color,rec.only_face,!rec.inverse_normal);
+        let (t1,t2) = make_plane((rec.x.0,rec.x.1 + rec.size.1,rec.x.2),(rec.size.0,0f64,rec.size.2),rec.color,rec.only_face,!rec.inverse_normal);
         triangles.push(t1);
         triangles.push(t2);
     }
